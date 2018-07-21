c73570e592656b3c61e796c26470f2c412ad7000
florian <florian@management.ad.flotastisch.net>
2018-07-21 23:25:54 +0200

    pylint: Found indentation with tabs instead of spaces (mixed-indentation)

--- punycode.py
+++ punycode.py
@@ -4,14 +4,13 @@
 import sys
 
 for line in sys.stdin:
-	domaininfos = line.strip().split(",")
+    domaininfos = line.strip().split(",")
 
-	try:
-		domain = domaininfos[0]
-		encodeddomain = domain.encode("idna")
-		domaininfos[0] = encodeddomain.decode("UTF-8")
-		print(",".join(domaininfos))
-	except UnicodeError:
-		# after fail just print it
-		print(line)
-
+    try:
+        domain = domaininfos[0]
+        encodeddomain = domain.encode("idna")
+        domaininfos[0] = encodeddomain.decode("UTF-8")
+        print(",".join(domaininfos))
+    except UnicodeError:
+        # after fail just print it
+        print(line)
